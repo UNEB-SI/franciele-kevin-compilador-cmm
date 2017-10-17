95bab48a00194e357b640703c12163137956ec57
Kevin Oliveira <kevin.oliveira@live.com>
2017-10-17 09:23:29 -0300

    Tabela de palavras reservadas e tabela de sinais

--- main.c
+++ main.c
@@ -4,5 +4,43 @@
 #include "my-lib.h"
 
 void main(){
-    printf("Hello World!\n");
+printf("Hello World!\n");
+
+printf("\nSINAIS\n");
+printf("SN_abreChaves -> %s\n",SN_TABLE[SN_abreChaves] );
+printf("SN_fechaChaves -> %s\n",SN_TABLE[SN_fechaChaves] );
+printf("SN_abreParenteses -> %s\n",SN_TABLE[SN_abreParenteses] );
+printf("SN_fechaParenteses -> %s\n",SN_TABLE[SN_fechaParenteses] );
+printf("SN_pontoEVirgula -> %s\n",SN_TABLE[SN_pontoEVirgula] );
+printf("SN_divisao -> %s\n",SN_TABLE[SN_divisao] );
+printf("SN_mutiplicacao -> %s\n",SN_TABLE[SN_mutiplicacao] );
+printf("SN_soma -> %s\n",SN_TABLE[SN_soma] );
+printf("SN_subtracao -> %s\n",SN_TABLE[SN_subtracao] );
+printf("SN_virgula -> %s\n",SN_TABLE[SN_virgula] );
+printf("SN_maiorQue -> %s\n",SN_TABLE[SN_maiorQue] );
+printf("SN_maiorIgualQue -> %s\n",SN_TABLE[SN_maiorIgualQue] );
+printf("SN_menorQue -> %s\n",SN_TABLE[SN_menorQue] );
+printf("SN_menorIgualQue -> %s\n",SN_TABLE[SN_menorIgualQue] );
+printf("SN_negacao -> %s\n",SN_TABLE[SN_negacao] );
+printf("SN_difente -> %s\n",SN_TABLE[SN_difente] );
+printf("SN_atribuicao -> %s\n",SN_TABLE[SN_atribuicao] );
+printf("SN_igualdade -> %s\n",SN_TABLE[SN_igualdade] );
+printf("SN_ouCondicional -> %s\n",SN_TABLE[SN_ouCondicional] );
+printf("SN_eCondiciona -> %s\n",SN_TABLE[SN_eCondicional] );
+
+printf("\nPALAVRAS RESERVADAS\n");
+printf("inteiro -> %s\n",PR_TABLE[inteiro]);
+printf("real -> %s\n",PR_TABLE[real]);
+printf("booleano -> %s\n",PR_TABLE[booleano]);
+printf("caracter -> %s\n",PR_TABLE[caracter]);
+printf("retorne -> %s\n",PR_TABLE[retorne]);
+printf("semretorno -> %s\n",PR_TABLE[semretorno]);
+printf("semparam -> %s\n",PR_TABLE[semparam]);
+printf("se -> %s\n",PR_TABLE[se]);
+printf("para -> %s\n",PR_TABLE[para]);
+printf("enquanto -> %s\n",PR_TABLE[enquanto]);
+printf("principal -> %s\n",PR_TABLE[principal]);
+
 }
+
+
--- my-lib.h
+++ my-lib.h
@@ -1,16 +1,89 @@
+// IDs
+char ID_TABLE[1000][100];
 
-int ID_TABLE;
+// Palavras reservadas
+enum PR{
+    inteiro=0,
+    real,
+    booleano,
+    caracter,
+    retorne,
+    semretorno,
+    semparam,
+    se,
+    para,
+    enquanto,
+    principal,
+};
+char PR_TABLE[][50]={
+    "inteiro",
+    "real",
+    "booleano",
+    "caracter",
+    "retorne",
+    "semretorno",
+    "semparam",
+    "se",
+    "para",
+    "enquanto",
+    "principal"
+};
 
-enum PR;
-int PR_TABLE;
+// Sinais
+enum SN {
+    SN_abreChaves = 0,
+    SN_fechaChaves,
+    SN_abreParenteses,
+    SN_fechaParenteses,
+    SN_pontoEVirgula,
+    SN_divisao,
+    SN_mutiplicacao,
+    SN_soma,
+    SN_subtracao,
+    SN_virgula,
+    SN_maiorQue,
+    SN_maiorIgualQue,
+    SN_menorQue,
+    SN_menorIgualQue,
+    SN_negacao,
+    SN_difente,
+    SN_atribuicao,
+    SN_igualdade,
+    SN_ouCondicional,
+    SN_eCondicional
+};
+char SN_TABLE[][50]={
+    "{",
+    "}",
+    "(",
+    ")",
+    ";",
+    "/",
+    "*",
+    "+",
+    "-",
+    ",",
+    ">",
+    ">=",
+    "<",
+    "<=",
+    "!",
+    "!=",
+    "=",
+    "==",
+    "||",
+    "&&"
+};
 
-enum SN;
-int SN_TABLE;
+// Constantes
+int CT_I_TABLE[100];
+float CT_R_TABLE[100];
+char CT_C_TABLE[100];
 
-int CT_I_TABLE;
-float CT_R_TABLE;
-char CT_C_TABLE[];
-
+// Literais
 char LT[][100];
 
-#define EOF 
+// Outros
+#define C_NovaLinha '\n';
+#define C_Tab '\t';
+#define C_Espaco '\b';
